7ce8d87297e45e0094ad2d89b1357bea27abe2ea
marcelo-bn <marcelobittencourtx@gmail.com>
2018-10-06 11:37:41 -0300

    Atualizando Principal.java

--- veiculos-polimorfismo/src/main/java/poo/Principal.java
+++ veiculos-polimorfismo/src/main/java/poo/Principal.java
@@ -7,37 +7,37 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Alguns testes para entender...\n");
 
-//        VeiculoTerrestre a = new Ferrari("Testarossa");
-//        VeiculoTerrestre b = new Panther("Panther2");
+        VeiculoTerrestre a = new Ferrari("Testarossa",0);
+        VeiculoTerrestre b = new Panther("Panther2",0);
         VeiculoTerrestre c = new Pampa("Pampa Guia", 0);
-//        VeiculoMarinho   d = new Panther("Panther python");
-//        VeiculoAnfibio e = new Panther("Panther watercar");
-//        Veiculo f = new Ferrari("F430");
-//        Veiculo g = new Panther("Panther Fusc");
-//        Veiculo h = new Pampa("LS");
+        VeiculoMarinho   d = new Panther("Panther python",0);
+        VeiculoAnfibio e = new Panther("Panther watercar",0);
+        Veiculo f = new Ferrari("F430",0);
+        Veiculo g = new Panther("Panther Fusc",0);
+        Veiculo h = new Pampa("LS",0);
 
 
-//        a.acelerar(10);
-//        b.acelerar(10);
-          c.acelerar(10);
-          c.frear(10);
-          //c.acelerar(20);
-          System.out.println(((Pampa) c).ativarDesativarTracao());
-          System.out.println(((Pampa) c).ativarDesativarTracao());
-//        e.acelerar(10);
+        a.acelerar(10);
+        b.acelerar(10);
+        c.acelerar(10);
+        c.frear(10);
+        c.acelerar(20);
+        System.out.println(((Pampa) c).ativarDesativarTracao());
+        System.out.println(((Pampa) c).ativarDesativarTracao());
+        e.acelerar(10);
 
-//        ((Ferrari) a).abrirCapota();
-//
-//        d.esvaziarLastro();
-//        e.esvaziarLastro();
-//
-//        ((Ferrari) f).acelerar(1);
-//
-//        Ferrari i = (Ferrari) f;
-//        i.abrirCapota();
-//        i.acelerar(10);
-//
-//        ((Ferrari) f).ligarFarolNeblina();
+        ((Ferrari) a).abrirCapota();
+
+        d.esvaziarLastro();
+        e.esvaziarLastro();
+
+        ((Ferrari) f).acelerar(1);
+
+        Ferrari i = (Ferrari) f;
+        i.abrirCapota();
+        i.acelerar(10);
+
+        ((Ferrari) f).ligarFarolNeblina();
 
 
     }
